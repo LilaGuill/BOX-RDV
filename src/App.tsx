bfbf6b87b4bbbc0d02ffd89c0b6c162bd80728db
LilaGuill <lila.guillermic@gmail.com>
2023-02-25 13:56:22 +0100

    feat/rdv-screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
 import { GlobalStyle } from "./theme/styled-components"
+import { RendezVous } from "./screens"
 import theme from "./theme/theme"
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      Hello Rendez-vous!
+      <Router>
+        <Routes>
+          <Route path="/" element={<RendezVous />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   )
 }
---
+++ src/screens/rendezVous/index.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Text } from "../../components"
+
+const RendezVous = () => {
+  return (
+    <div>
+      <Text>RDV</Text>
+    </div>
+  )
+}
+
+export default RendezVous
