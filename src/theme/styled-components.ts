8b4a1d3f8c32c10320e0d361dc0d634ebff3ce35
LilaGuill <lila.guillermic@gmail.com>
2023-02-25 12:36:53 +0100

    feat/set-up

---
+++ src/theme/styled-components.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    color: #34423e;
+    font-size: 13px;
+    font-family: "Open Sans", sans-serif;
+  }
+
+`
---
+++ src/theme/theme.ts
@@ -0,0 +1,27 @@
+const colors = {
+  background: "#F6F7F8",
+  blue: "#2C5282",
+  blueLight: "##63B3ED",
+  green: "#48BB78",
+  grey000: "#F9F9F9",
+  grey100: "##F6F7F8",
+  grey200: "#ECEEED",
+  grey800: "#5F706A",
+  grey900: "#34423E",
+  red: "#F56565",
+  white: "#FFFFFF",
+}
+
+const typography = {
+  fontFamilies: {},
+  fontWeights: {},
+  fontSizes: {},
+}
+
+const theme = {
+  name: "Default theme",
+  colors,
+  typography,
+}
+
+export default theme
